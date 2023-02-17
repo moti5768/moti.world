b4292688cf777317507294abf030428c91067601
moti5768 <103321115+moti5768@users.noreply.github.com>
2023-02-17 15:51:37 +0900

    Delete search.js

--- search.js
+++
@@ -1,9 +0,0 @@
-function check(){
-  if (search_form.search.value == "#test#"){
-    alert('コマンドが実行されました!')
-    return true;
-  }else{(search_form.search.value == "")
-  alert('コマンドが正しく入力されていません!')
-    return false;
-  }
-}
