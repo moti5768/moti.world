1da05e5993bdea84b887fa8023de288be1d78235
moti5768 <103321115+moti5768@users.noreply.github.com>
2023-04-25 20:31:46 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+let test = 'test hello';
+const test2 = 'test2 hello';
+console.log(test);
+console.log(test2);
+
+function alertbutton(){
+    alert(test);
+}
+
+function alertbutton2(){
+    alert(test2);
+}
+
+function color_red(){
+    document.getElementById("color").style.color = "red";
+}
+
+function backgroundColor_red(){
+    document.getElementById("backgroundColor").style.backgroundColor = "red";
+}
+
+function color_blue(){
+    document.getElementById("color").style.color = "blue";
+}
+
+function backgroundColor_blue(){
+    document.getElementById("backgroundColor").style.backgroundColor = "blue";
+}
+
+function color_black(){
+    document.getElementById("color").style.color = "black";
+}
+
+function backgroundColor_white(){
+    document.getElementById("backgroundColor").style.backgroundColor = "white";
+}
