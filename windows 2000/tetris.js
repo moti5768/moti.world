68c1b53cf5ff94902a94db076c93fb5596d1a74b
moti5768 <103321115+moti5768@users.noreply.github.com>
2024-01-16 16:41:39 +0900

    Update tetris.js

--- windows 2000/tetris.js
+++ windows 2000/tetris.js
@@ -1,7 +1,7 @@
 class tetris {
     constructor() {
-        this.stageWidth = 10;
-        this.stageHeight = 20;
+        this.stageWidth = 20;
+        this.stageHeight = 40;
         this.stageCanvas = document.getElementById("stage");
         this.nextCanvas = document.getElementById("next");
         let cellWidth = this.stageCanvas.width / this.stageWidth;
