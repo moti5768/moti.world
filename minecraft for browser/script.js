8eb28250a9f7e8ba2ffb496e84a71f25b8ac91cc
moti5768 <103321115+moti5768@users.noreply.github.com>
2025-06-01 01:46:45 +0900

    Update script.js

--- minecraft for browser/script.js
+++ minecraft for browser/script.js
@@ -1,4 +1,4 @@
-import * as THREE from "./build/three.module.js";
+import * as THREE from './build/three.module.js';
 import { BufferGeometryUtils } from './jsm/utils/BufferGeometryUtils.js';
 "use strict";
 
